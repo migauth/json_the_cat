8362fc9547ecefd2ffeb83d789a045b4df935d3e
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-16 13:06:21 -0800

    add try catch cases for invalid url and breed not existing

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,8 +5,23 @@ let argArr = arg.slice(2);
 // Import request library
 const request = require("request");
 
-const urlData = argArr[0]
+// The breed argument in command line input
+const breed = argArr[0];
+
+// Api link for fetching cat data
+let urlData = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 
-
-const info = request()
+try { // Check if valid URL
+  request(urlData, (error, response, body) => {
+    const data = JSON.parse(body);
+    try { // Check if breed exists
+      console.log(data[0].description);
+    } catch (error) {
+      console.log("Error: Breed not found!");
+    }
+  });
+  
+} catch (error) {
+  console.log("Error: Invalid URL!");
+}
